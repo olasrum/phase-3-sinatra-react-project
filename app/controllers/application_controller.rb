0c6f86c5c2dbda3146dcfa227822f3f47dbf19da
olasrum <mursalo@gmail.com>
2023-01-22 11:41:22 +1000

    progress update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/weekly_workout" do
+  get "/exercises" do
     exercises = Exercise.all
     exercises.to_json
   end
 
-  get "/categories" do 
+  get "/exercises_categories" do 
     categories = Category.all
     categories.to_json
   end
